661fe0c5f7d5c7b90165a4e049121a3846c58c7a
JeanMax <mc@rmrf.fr>
2023-11-24 18:03:49 +0100

    WIP

--- cemantax.py
+++ cemantax.py
@@ -4,6 +4,7 @@ import sys
 import os
 import time
 import inspect
+import random
 
 import requests
 from gensim.models import KeyedVectors
@@ -18,7 +19,8 @@ MODEL_PATH = os.path.join(
 )
 DIC_PATH = os.path.join(
     HERE,
-    "liste_francais_ascii_bis.txt"
+    # "liste_francais_ascii_bis.txt"
+    "superdic.txt"
 )
 
 URL = "https://cemantix.herokuapp.com/score"
@@ -72,13 +74,64 @@ def load_model(model_path):
     )
 
 
+# SOLVER
+def get_word(prev_word, score, dic, score_dic):
+    sorted_w = sorted(score_dic, key=lambda x: score_dic[x], reverse=True)
+    pos = [w for w in sorted_w if score_dic[w] > 0.2][:12]
+    neg = [w for w in sorted_w[::-1] if score_dic[w] < 0.1][:12]
+    if not pos and not neg:
+        if score > 0:
+            pos.append(prev_word)
+        else:
+            neg.append(prev_word)
+
+    words = model.most_similar(
+        positive=pos,
+        negative=neg,
+        topn=len(dic)
+    )
+
+    # print("words:", words[:3])      # DEBUG
+    for w, s in words:
+        if w in dic and w not in score_dic.keys():
+            return w
+    raise Exception("prout")
+
+
+def solve(model, dic):
+    score_dic = {}
+
+    # first try at random:
+    random.shuffle(dic)
+    w = dic[0]
+
+    while 42:
+        print("w:", w)      # DEBUG
+        req = post_word(w)
+        print(req)      # DEBUG
+        score = req["score"]
+        score_dic[w] = score
+        if score == 1:
+            break
+
+        # sorted_w = sorted(score_dic, key=lambda x: score_dic[x], reverse=True)
+        # best, worst = sorted_w[0], sorted_w[-1]
+        # if score < 0.2 and score_dic[best] < 0.3 and score_dic[worst] < 0:
+        #     w = worst
+        #     print("* using previous WORST *")
+        # elif score_dic[best] > 0.3:
+        #     w = best
+        #     print("* using previous BEST *")
+        w = get_word(w, score, dic, score_dic)
+
+        time.sleep(0.1)
+
+    print(f"YAY! '{w}' found in {len(score_dic)} tries")
+    return score_dic
+
+
 if __name__ == "__main__":
-    voc = load_dic(DIC_PATH)
+    dic = load_dic(DIC_PATH)
     model = load_model(MODEL_PATH)
 
-    voc_dic = {}
-    for w in voc:
-        try:
-            voc_dic[w] = model.most_similar(w)
-        except KeyError:
-            warn(f"could find word '{w}' in model")
+    solve(model, dic)
